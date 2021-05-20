094ebfd7b3b5543419511ed697377b115ecb42e4
Rafael Aparecido Martins Frade <rafaelfrade@MacBook-Pro-de-Rafael.local>
2021-05-20 09:48:05 +0200

    add import to data.table

--- R/forint.R
+++ R/forint.R
@@ -1,8 +1,8 @@
 #' This function adds HUF at the end of the value passed as argument
 #' @param numeric value to be formatted
-#' @export
-#' @examples
-#' forint(10) -> 10 HUF
+#' @importFrom scales dollar
+#' @export 
+#' @examples forint(10) -> 10 HUF
 forint <- function(x){
   scales::dollar(x, prefix = '', suffix = ' HUF')
 }
--- R/get_bitcoin_price.R
+++ R/get_bitcoin_price.R
@@ -1,15 +1,16 @@
-#' This returns the current price of bitcoins in dollars
-#' @examples
-#' get_bitcoin_price()
+#' This returns the current price of bitcoins in US dollars
+#' @export
+#' @importFrom logger log_info log_error
+#' @importFrom binancer binance_coins_prices
+#' @import data.table
 get_bitcoin_price <- function() {
   tryCatch({
-    btcusdt <- binance_coins_prices()[symbol == 'BTC', usd]
-    assert_number(btcusdt, lower = 1000)
-    log_info('The current Bitcoin price is ${btcusdt}')
+    btcusdt <- binancer::binance_coins_prices()[symbol == 'BTC']
+    logger::log_info('The current Bitcoin price is ${btcusdt}')
     btcusdt
   },
   error = function(e) {
-    log_error(e$message)
+    logger::log_error(e$message)
     Sys.sleep(1)
     get_bitcoin_price()
   })
